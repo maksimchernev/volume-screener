3d1551bf4aeaa4a6545dcde0bac9db29a248a8c8
Maksim Chernev <m.chernev@mail.ru>
2023-10-08 15:39:16 +0300

    try ccatch

--- bot.js
+++ bot.js
@@ -11,41 +11,50 @@ const binance = new ccxt.binance();
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const sync = async () => {
-  let serverTime = await binance.fetchTime();
-  let timeTillTheEndOfTheDay =
-    1000 * 60 * 60 * 4 - (serverTime % (1000 * 60 * 60 * 4));
-  await wait(timeTillTheEndOfTheDay - 30 * 60 * 1000); // delay to get most accurate data in a minute frame
+  try {
+    let serverTime = await binance.fetchTime();
+    let timeTillTheEndOfTheDay =
+      1000 * 60 * 60 * 4 - (serverTime % (1000 * 60 * 60 * 4));
+    await wait(timeTillTheEndOfTheDay - 30 * 60 * 1000); // delay to get most accurate data in a minute frame
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 const screen = async (chatId) => {
-  while (true) {
-    const responseTickers = await binance.fetchTickers();
-    for (let ticker in responseTickers) {
-      if (ticker.match("USDT")) {
-        const responseOHLCV = await binance.fetchOHLCV(
-          responseTickers[ticker].symbol,
-          "1d",
-          undefined,
-          15
-        );
-        let sum = 0;
-        for (let i = responseOHLCV.length - 2; i >= 0; i--) {
-          sum = sum + responseOHLCV[i][5];
-        }
-        let averageVolume = sum / 14;
-        const diff = responseOHLCV[responseOHLCV.length - 1][5] / averageVolume;
-        console.log("working", new Date());
-        if (diff > 3) {
-          bot.sendMessage(
-            chatId,
-            `${ticker} ${Math.floor(diff)} times increase in volume`
+  try {
+    while (true) {
+      const responseTickers = await binance.fetchTickers();
+      for (let ticker in responseTickers) {
+        if (ticker.match("USDT")) {
+          const responseOHLCV = await binance.fetchOHLCV(
+            responseTickers[ticker].symbol,
+            "1d",
+            undefined,
+            15
           );
-        }
+          let sum = 0;
+          for (let i = responseOHLCV.length - 2; i >= 0; i--) {
+            sum = sum + responseOHLCV[i][5];
+          }
+          let averageVolume = sum / 14;
+          const diff =
+            responseOHLCV[responseOHLCV.length - 1][5] / averageVolume;
+          console.log("working", new Date());
+          if (diff > 3) {
+            bot.sendMessage(
+              chatId,
+              `${ticker} ${Math.floor(diff)} times increase in volume`
+            );
+          }
 
-        await wait(3000);
+          await wait(3000);
+        }
       }
+      await sync();
     }
-    await sync();
+  } catch (e) {
+    console.log(e);
   }
 
   //console.log(response)
