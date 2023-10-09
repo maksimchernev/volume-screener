702366c0258907034be2f978361fc882bdd1bce9
Чернев Максим <cmd@coolclever.ru>
2023-10-09 17:19:48 +0300

    logs writing, proper error handling

--- bot.js
+++ bot.js
@@ -1,70 +1,104 @@
 "use strict";
-
 require("dotenv").config();
 const ccxt = require("ccxt");
+const fs = require("fs");
 const TelegramBot = require("node-telegram-bot-api");
 const token = process.env.TG_KEY;
 const bot = new TelegramBot(token, { polling: true });
 
 const binance = new ccxt.binance();
+const wait = (ms = 4000) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-const sync = async () => {
+const sync = async (chatId) => {
   try {
-    let serverTime = await binance.fetchTime();
-    let timeTillTheEndOfTheDay =
+    const serverTime = await binance.fetchTime();
+    const timeTillTheEndOfTheDay =
       1000 * 60 * 60 * 4 - (serverTime % (1000 * 60 * 60 * 4));
-    await wait(timeTillTheEndOfTheDay - 30 * 60 * 1000); // delay to get most accurate data in a minute frame
+    const howLongWaitMs = timeTillTheEndOfTheDay - 30 * 60 * 1000;
+
+    const message = `SYNC. Now: ${new Date()}, wait: ${
+      howLongWaitMs / 1000 / 60
+    }min \r\n`;
+    writeInfo(message);
+    bot.sendMessage(chatId, message);
+    await wait(howLongWaitMs); // delay to get most accurate data in a minute frame
   } catch (e) {
     console.log(e);
   }
 };
 
-const screen = async (chatId) => {
-  try {
-    while (true) {
-      const responseTickers = await binance.fetchTickers();
-      for (let ticker in responseTickers) {
-        if (ticker.match("USDT")) {
-          const responseOHLCV = await binance.fetchOHLCV(
-            responseTickers[ticker].symbol,
-            "1d",
-            undefined,
-            15
-          );
-          let sum = 0;
-          for (let i = responseOHLCV.length - 2; i >= 0; i--) {
-            sum = sum + responseOHLCV[i][5];
-          }
-          let averageVolume = sum / 14;
-          const diff =
-            responseOHLCV[responseOHLCV.length - 1][5] / averageVolume;
-          console.log("working", new Date());
-          if (diff > 3) {
-            bot.sendMessage(
-              chatId,
-              `${ticker} ${Math.floor(diff)} times increase in volume`
-            );
-          }
-
-          await wait(3000);
-        }
-      }
-      await sync();
+const writeInfo = (info) => {
+  console.log(info);
+  fs.writeFile("./errors.txt", `${info} \r\n `, { flag: "a+" }, (err) => {
+    if (err) {
+      console.error(err);
     }
-  } catch (e) {
-    console.log(e);
-  }
+  });
+};
 
-  //console.log(response)
+const getTickers = async () => {
+  try {
+    return await binance.fetchTickers();
+  } catch (e) {
+    writeInfo(`fetchTickers ${e}`);
+  }
+};
+
+const getOHLCV = async (symbol, tf, limit) => {
+  try {
+    return await binance.fetchOHLCV(symbol, tf, undefined, limit);
+  } catch (e) {
+    writeInfo(`fetchOHLCV ${e}`);
+  }
+};
+
+const screen = async (chatId) => {
+  while (true) {
+    let responseTickers = await getTickers();
+    if (!responseTickers) continue;
+
+    for (let ticker in responseTickers) {
+      if (!ticker.match("USDT")) {
+        continue;
+      }
+      const responseOHLCV = await getOHLCV(
+        responseTickers[ticker].symbol,
+        "1d",
+        15
+      );
+
+      if (!responseOHLCV?.length) {
+        await wait();
+        continue;
+      }
+
+      let sum = 0;
+
+      for (let i = responseOHLCV.length - 2; i >= 0; i--) {
+        sum = sum + responseOHLCV[i][5];
+      }
+
+      let averageVolume = sum / 14;
+      const diff = responseOHLCV[responseOHLCV.length - 1][5] / averageVolume;
+
+      if (diff > 3) {
+        bot.sendMessage(
+          chatId,
+          `${ticker} ${Math.floor(diff)} times increase in volume`
+        );
+      }
+
+      await wait();
+    }
+    await sync(chatId);
+  }
 };
 
 bot.onText(/\/start/, async (msg) => {
   let chatId = msg.chat.id;
   bot.sendMessage(
     msg.chat.id,
-    "At the end of 1d candle I will start screening. Patience...."
+    "Работаю при запуске и в конце дня UTC (3am МСК). Я стал умнее, правда "
   );
   await screen(chatId);
 });
